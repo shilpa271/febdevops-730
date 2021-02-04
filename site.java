7b542574c06de5def842c935b2e4b3844050c91b
shilpa <shilpaparameshwara1997@gmail.com>
2021-02-03 22:18:31 -0500

    this is my initial commit

---
+++ site.java
@@ -0,0 +1,23 @@
+
+public class site {
+	public void father()
+	{
+		System.out.println("fathers site");
+		
+	}
+	public void mother()
+	{
+		System.out.println("mothers site");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+site obj=new site();
+site obj1=new site();
+obj1.mother();
+obj.father();
+
+	}
+
+}
