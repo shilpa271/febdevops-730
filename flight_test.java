30aaf4b703a4e9de37defdebb46a058960501828
shilpa <shilpaparameshwara1997@gmail.com>
2021-02-07 20:03:21 -0500

    this is aggignment java files

---
+++ flight_test.java
@@ -0,0 +1,16 @@
+
+public class flight_test {
+
+	public static void main(String[] args) {
+		flight flight1=new flight("united airlines","India","USA",802);
+		flight flight2=new flight("AirIndia","uganda","India",119);
+		flight flight3=new flight("Emirates","Dubai","England",479);
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(flight1);
+		System.out.println(flight2);
+		System.out.println(flight3);
+	}
+
+}
